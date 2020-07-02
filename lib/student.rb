0aa838736b018e9781df7d277462689c3da177a4
interactive-directory-1839 <benthedunn@gmail.com>
2020-07-02 17:13:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,6 +25,23 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql = <<-SQL
+      DELETE * FROM TABLE WHERE TABLE EXISTS students (
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade, id)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
   def self.all
     @@all
   end
