2d0f873fcd70db60383b78ebbbc00c092b605297
interactive-directory-1839 <benthedunn@gmail.com>
2020-07-02 17:27:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
       VALUES (?, ?)
       SQL
 
-      DB[:conn].execute(sql, self.name, self.grade)
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   def self.all
