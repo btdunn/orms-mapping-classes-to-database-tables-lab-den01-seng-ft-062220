f449b284c7cc1d7d92ce544003786b5245e66b43
interactive-directory-1839 <benthedunn@gmail.com>
2020-07-02 16:48:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,18 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+  attr_accessor :name, :grade
+
+  @@all = []
+
+  def initialize name, grade
+    @name = name
+    @grade = grade
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
